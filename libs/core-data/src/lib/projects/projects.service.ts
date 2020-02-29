7606f30b347f76dd850a6a46ecd202c19ecd5b69
Steve Witman <stevewitman@gmail.com>
2020-02-29 09:12:08 -0700

    Added crud methods to projects service

--- libs/core-data/src/lib/projects/projects.service.ts
+++ libs/core-data/src/lib/projects/projects.service.ts
@@ -1,9 +1,44 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { Project } from './project.model'
+
+const BASE_URL = "https://bb-base-server.herokuapp.com";
+const MODEL = "projects"
 
 @Injectable({
   providedIn: 'root'
 })
 export class ProjectsService {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  all(): Observable<Project[]> {
+    return this.http.get<Project[]>(this.url());
+  }
+
+  findOne(id: string): Observable<Project> {
+    return this.http.get<Project>(this.urlWithId(id));
+  }
+
+  create(project: Project): Observable<Project> {
+    return this.http.post<Project>(this.url(), project);
+  }
+
+  update(project: Project): Observable<Project> {
+    return this.http.patch<Project>(this.urlWithId(project.id), project);
+  }
+
+  delete(project: Project): Observable<Project> {
+    return this.http.delete<Project>(this.urlWithId(project.id));
+  }
+
+  private url() {
+    return `${BASE_URL}/${MODEL}`;
+  }
+
+  private urlWithId(id: string) {
+    return `${url()}/${id}`
+  }
+
 }
